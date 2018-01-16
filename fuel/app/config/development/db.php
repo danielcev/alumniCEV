86daafbbe09d6fd8fa2fa6b6c0dbabdd2078fabd
danielcev <daniel_plata_apps1m1617@cev.com>
2018-01-16 11:39:17 +0100

    produccion

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost:8889;dbname=alumniCEV',
-			'username'   => 'root',
-			'password'   => 'root',
+			'dsn'        => 'mysql:host=localhost;dbname=danip',
+			'username'   => 'danip',
+			'password'   => '7y1RXtIMBc4ynUvz',
 		),
 	),
 );
