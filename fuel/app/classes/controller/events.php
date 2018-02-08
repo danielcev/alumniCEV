382390952e93efa7e591bf77846eeb4fe0f7cc11
danielcev <daniel_plata_apps1m1617@cev.com>
2018-02-08 09:36:31 +0100

    Duplicados

--- fuel/app/classes/controller/events.php
+++ fuel/app/classes/controller/events.php
@@ -167,7 +167,7 @@ class Controller_Events extends Controller_Rest
                 if ($typeDB == null) {
                     return $this->createResponse(400, 'Parametro type no valido');
                 }
-                $query = \DB::query('SELECT events.*,types.name FROM belong
+                $query = \DB::query('SELECT DISTINCT events.*,types.name FROM belong
                                         JOIN users ON belong.id_user = users.id
                                         JOIN groups ON groups.id = belong.id_group
                                         JOIN asign ON asign.id_group = groups.id
